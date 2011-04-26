c0478b7d2bf48ef0d38fdfbb631189570184e06c
Brock Whitten <brock@sintaxi.com>
2011-04-26 14:38:00 -0700

    initial commit

---
+++ public/application.js
@@ -0,0 +1,21 @@
+var q= function(s){ return document.getElementById(s) }
+
+function send(){
+  var input = q('text');
+  socket.send(input.value);
+  input.value = '';
+}
+
+var socket = new io.Socket(null, {
+  port: 8001,
+  rememberTransport: false
+});
+
+socket.on('message', function(message){
+  var el = document.createElement('p');
+  el.innerHTML = message;
+  q('chat').appendChild(el);
+  window.scrollBy(0, 9000000)
+});
+
+socket.connect();
